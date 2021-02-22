7155fa37d5e2fc4217b26ba503c9dc034b7e3084
Felix Cremer <felix.cremer@uni-jena.de>
2021-02-22 14:06:53 +0100

    Experimental speed up to test the benchmarkCI Setup
    This is just to test, whether the github action setup works.
    I made the array in the benchmark smaller by a factor of 100.

--- benchmark/bench_mapcube.jl
+++ benchmark/bench_mapcube.jl
@@ -7,7 +7,7 @@ using Random
 
 suite = BenchmarkGroup()
 
-suite["small"] = @benchmarkable mapslices(identity, YAXArray(a), dims="Dim_1") setup=(a=rand(1000,1000,10))
+suite["small"] = @benchmarkable mapslices(identity, YAXArray(a), dims="Dim_1") setup=(a=rand(10,10,10))
 
 end
 BenchMapCube.suite
